eeebb5317423bf2a7452d3d0bb7f1ff065c32d69
pommevilla <pvillanueva13@gmail.com>
2022-11-17 10:51:45 -0600

    Combine all dly files, add to Snakemake

--- code/read_dly_files.R
+++ code/read_dly_files.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(glue)
+library(lubridate)
 
 quadruplet <- function(x) {
     c(glue("VALUE{x}"), glue("MFLAG{x}"), glue("QFLAG{x}"), glue("SFLAG{x}"))
@@ -8,19 +9,34 @@ quadruplet <- function(x) {
 widths <- c(11, 4, 2, 4, rep(c(5, 1, 1, 1), 31))
 headers <- c("ID", "YEAR", "MONTH", "ELEMENT", unlist(map(1:31, quadruplet)))
 
-dly_files <- list.files("data/ghcnd_all", full.names = TRUE)
+%>% 
+    map_dfr(., ~ read_tsv(archive_read("write_dir.tar.gz", .x)))
 
-read_fwf(
-    dly_files,
-    fwf_widths(widths, headers),
-    na = c("NA", "-9999", ""),
-    col_select = c(ID, YEAR, MONTH, ELEMENT, starts_with("VALUE"))
-) %>%
-    rename_all(tolower) %>%
-    filter(element == "PRCP") %>%
-    select(-element) %>%
-    pivot_longer(
-        cols = starts_with("value"),
-        names_to = "day",
-        values_to = "prcp"
-    )
+dly_files <- archive("data/ghcnd_all.tar.gz") %>% 
+    filter(str_ends(path, ".dly")) %>% 
+    pull("path") 
+
+read_fwf("data/ghcnd_cat.gz",
+        fwf_widths(widths, headers),
+        na = c("NA", "-9999", ""),
+        col_types = cols(.default = col_character()),
+        col_select = c(ID, YEAR, MONTH, ELEMENT, starts_with("VALUE")))
+        
+
+        %>%
+        rename_all(tolower) %>%
+        filter(element == "PRCP") %>%
+        select(-element) %>%
+        pivot_longer(
+            cols = starts_with("value"),
+            names_to = "day",
+            values_to = "prcp"
+        ) %>% 
+        drop_na() %>% 
+        mutate(day = str_replace(day, "value", ""),
+            date = ymd(glue("{year}-{month}-{day}")),
+            prcp = as.numeric(prcp) / 100 # prcp now in cm
+        )  %>% 
+        select(id, date, prcp) %>% 
+        write_tsv("data/composite_dly.tsv")
+    
